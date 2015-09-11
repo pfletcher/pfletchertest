e36de9a6e3300c132f490affa0a65fdd928abd62
JM Perlman <JMPerlman@users.noreply.github.com>
2015-09-11 13:25:29 -0700

    Update server.js
    changing output

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var http = require('http');
 // The  HTTP server will respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("\n Hello World (modified 9/11 13:08) \n");
+  response.end("\n Hello World (modified 9/11 13:08 by someone else.) \n");
 });
 
 // Listen to the port being used by this app. The call to process.env.PORT will
