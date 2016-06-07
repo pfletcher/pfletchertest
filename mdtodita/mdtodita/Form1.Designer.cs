ed49e719bb701614fc7aa25db49d812c03b8ef77
Patrick Fletcher <patrick.d.fletcher@gmail.com>
2016-06-07 13:13:34 -0700

    Initial add of md to dita project

---
+++ mdtodita/mdtodita/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace mdtodita
+{
+    partial class MdToDitaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextConsole = new System.Windows.Forms.RichTextBox();
+            this.buttonReadFiles = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextConsole
+            // 
+            this.richTextConsole.Location = new System.Drawing.Point(13, 90);
+            this.richTextConsole.Name = "richTextConsole";
+            this.richTextConsole.Size = new System.Drawing.Size(259, 160);
+            this.richTextConsole.TabIndex = 0;
+            this.richTextConsole.Text = "";
+            // 
+            // buttonReadFiles
+            // 
+            this.buttonReadFiles.Location = new System.Drawing.Point(13, 13);
+            this.buttonReadFiles.Name = "buttonReadFiles";
+            this.buttonReadFiles.Size = new System.Drawing.Size(259, 23);
+            this.buttonReadFiles.TabIndex = 1;
+            this.buttonReadFiles.Text = "Read files.xml";
+            this.buttonReadFiles.UseVisualStyleBackColor = true;
+            this.buttonReadFiles.Click += new System.EventHandler(this.buttonReadFiles_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(13, 43);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(259, 23);
+            this.buttonConvert.TabIndex = 2;
+            this.buttonConvert.Text = "Convert to DITA";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // MdToDitaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.buttonReadFiles);
+            this.Controls.Add(this.richTextConsole);
+            this.Name = "MdToDitaForm";
+            this.Text = "MD to Dita";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextConsole;
+        private System.Windows.Forms.Button buttonReadFiles;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ mdtodita/mdtodita/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml;
+
+namespace mdtodita
+{
+    public partial class MdToDitaForm: Form
+    {
+        List<documentToConvert> Documents = new List<documentToConvert>();
+        public MdToDitaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReadFiles_Click(object sender, EventArgs e)
+        {
+            var reader = new XmlTextReader("files.xml");
+            while(reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    
+                    case XmlNodeType.Element:
+                        //appendLine(reader.Name);
+                        switch (reader.Name)
+                        {
+                            case "file":
+                                {
+                                    var curDocument = new documentToConvert();
+                                    while (reader.Read())
+                                    {
+                                        //Break out at end of file node (hack)
+                                        if (reader.NodeType == XmlNodeType.EndElement && reader.Name == "file")
+                                        {
+                                            break;
+                                        }
+                                        switch (reader.NodeType)
+                                        {
+                                            case XmlNodeType.Element:
+                                                switch (reader.Name)
+                                                {
+                                                    case "filename":
+                                                        reader.Read();
+                                                        curDocument.filename = reader.Value;
+                                                        break;
+                                                    case "topicId":
+                                                        reader.Read();
+                                                        curDocument.topicId = reader.Value;
+                                                        break;
+                                                    case "outputFilename":
+                                                        reader.Read();
+                                                        curDocument.outputFilename = reader.Value;
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                    }
+                                    Documents.Add(curDocument);
+                                    appendLine(String.Format("Document read: {0} {1} {2}", curDocument.filename, curDocument.topicId, curDocument.outputFilename));
+                                }
+                                break;
+                        }
+                        break;
+                    case XmlNodeType.Text:
+                        //appendLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        //appendLine(reader.Name);
+                        break;
+
+                }
+            }
+        }
+        void appendLine(string text)
+        {
+            richTextConsole.AppendText(text + Environment.NewLine);
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            foreach (documentToConvert doc in Documents)
+            {
+                StreamReader file = new StreamReader(doc.filename);
+                List<String> inputFileLines = new List<String>();
+                List<String> outputFileLines = new List<String>();
+                string line;
+                bool inAList = false;
+
+                // Read file into list
+                while ((line = file.ReadLine()) != null) {
+                    inputFileLines.Add(line);
+                }
+
+                //Parse into output
+                for(int lineNumber = 0; lineNumber < inputFileLines.Count; lineNumber++)
+                {
+                    string lineToProcess = inputFileLines[lineNumber];
+
+                    //Is this part of a list (that starts with two spaces)?
+                    if (inAList)
+                    {
+                        //Still in list
+                        if (lineToProcess.StartsWith("  "))
+                        {
+                            outputFileLines.Add(String.Format("  <li>{0}</li>", lineToProcess.Substring(2)));
+                            continue;
+                        }
+                        else  //No longer in list
+                        {
+                            outputFileLines.Add("</ul>");
+                            inAList = false;
+                        }
+
+                    }
+                    
+                    if (lineToProcess.StartsWith("=="))     //Two-line header
+                    {
+                        outputFileLines.Add(String.Format("<p><b>{0}</b></p>", inputFileLines[lineNumber - 1]));
+                    }
+                    else if (lineToProcess.StartsWith("  "))    //Starting an unordered list
+                    {
+                        outputFileLines.Add("<ul>");
+                        outputFileLines.Add(String.Format("  <li>{0}</li>", lineToProcess.Substring(2)));
+                        inAList = true;
+                    }
+                    
+                }
+                appendLine(String.Format("Done reading {0}!", doc.topicId));
+            }
+            
+        }
+    }
+    class documentToConvert
+    {
+        public string filename { get; set; }
+        public string topicId { get; set; }
+        public string outputFilename { get; set; }
+    }
+}
